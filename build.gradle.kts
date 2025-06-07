314da3abde4b2b057b5aaab1e12ea51f4a34583a
TechnicallyCoded <technicallycoded@gmail.com>
2025-06-07 18:45:44 +0200

    Change to gradle

---
+++ build.gradle.kts
@@ -0,0 +1,62 @@
+plugins {
+    `java-library`
+    `maven-publish`
+}
+
+group = "com.tcoded.lightlibs"
+version = "0.0.5"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.jetbrains:annotations:23.0.0")
+}
+
+val enablePublishing: Boolean = project.findProperty("enablePublishing")?.toString()?.toBoolean() == true
+
+if (enablePublishing) {
+
+    publishing {
+        publications {
+            create<MavenPublication>("mavenJava") {
+                from(components["java"]) // Assumes this is a Java project
+
+                // Optional: add additional metadata
+//                pom {
+//                    name.set("My Library")
+//                    description.set("A simple library to publish to Reposilite")
+//                    url.set("https://example.com")
+//                }
+            }
+        }
+
+        repositories {
+            maven {
+                name = "reposilite"
+                url = uri("https://repo.tcoded.com/releases") // or /snapshots if applicable
+
+                credentials {
+                    username = System.getenv("REPOSILITE_USER")
+                        ?: error("Environment variable REPOSILITE_USER is not set")
+                    password = System.getenv("REPOSILITE_PASS")
+                        ?: error("Environment variable REPOSILITE_PASS is not set")
+                }
+
+                authentication {
+                    create<BasicAuthentication>("basic")
+                }
+            }
+        }
+    }
+
+    tasks.assemble {
+        dependsOn(tasks.publishToMavenLocal)
+    }
+}
